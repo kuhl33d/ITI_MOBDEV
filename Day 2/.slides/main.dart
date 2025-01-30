950aca0473d6b798b0181828a24799ba7e3b33de
kuhl33d <khaled.abdelnasser1@msa.edu.eg>
2025-01-30 15:31:36 +0200

    added day 2

---
+++ Day 2/.slides/main.dart
@@ -0,0 +1,206 @@
+void main() {
+  //function body
+  
+//   int x=5;
+// //   x=3.6;
+//   double y=3.6;
+//   String name="omar";
+//   name='mahmoud';
+//   print('name = $name');
+  
+//   const age; //error
+  
+//   final age=30; //runtime constant
+// //   age=13;
+  
+//   final degree;
+  
+//   print(age);
+  
+//   const fullname='ahmed ali';
+//   fullname='ramy ali'; //error
+  
+//   print(fullname);
+  
+  
+//   print(y);
+  
+  
+  
+//   strongly typed language
+  
+//   var x='amira';
+//   x=88;
+  
+  
+//   dynamic x=33; //any
+//   x='ali';
+//   x=true;
+//   print(x);
+  
+//   if statment
+//   int stdDegree=20;
+  
+//   int num1=-3;
+  
+//   if(num1>0){
+//     print('Positive');
+//   } else if(num1<0){
+//     print('Negative');
+//   }else{
+//     print('Zero');
+//   }
+  
+//   if(stdDegree>50){
+//     print('pass');
+//   }else{
+//     print('fail');
+//   }
+  
+  //   switch case
+  
+//   String role='Admin';
+  
+//   switch(role){
+//     case 'Admin':
+//       print('Access granted:Admin');
+//       break;
+//     case 'user':
+//       print('Access granted:user');
+//       break;  
+//     case 'custmor':
+//       print('Access granted:custmor');
+//       break;  
+//   }
+  
+//   Loops
+  
+//    for(int i=0;i<5;i++){
+//      print('count: $i');
+//    }
+  
+//   int counter=0;
+// //   infinity loop
+//   while(counter<=5){
+//     print('count: $counter');
+//     counter++;
+//   }
+  
+//   do{
+//     print('count: $counter');
+//     counter++;
+    
+//   }while(counter==3);
+  
+  
+//   while(true){
+    
+//   }
+  
+
+  //  operators
+//   assignment operator
+//   int x=3;
+//   int y=4;
+//   bool choice=true;
+//   double n=x as double;
+//   int w=n.toInt(); //Explicit
+//   print(n);
+//   x/=x;
+//   print(x);
+//   x++;
+//   print(x);
+//   x/=3; //x=x+3
+//   print(x);
+//   print(x+y);
+//   print(x-y);
+//   print(x*y);
+//   print(x/y);
+  
+//   print(x>3 && y<2);//true->all true
+  
+//   print(x>3 || y<2); //false=>all false
+  
+//   print(!choice);
+//   comparison operator
+//   print(x==3);
+//   ternary operator
+
+//    x==3?print('x is equal to 3'):print('Not Equal');
+  
+//   print(x==3?'x is equal to 3':'Not Equal');
+  
+
+    //at index 0
+//   length
+//   referance type
+//   List x=[2,3,4,1,2,3,3]; //memory address
+//   List y=[2,3,4,1];  //memory address
+  
+//   print(x);
+  
+//   print(x==y);
+  
+//   x[2]=14;
+//   x.add(13);
+//   x.remove(3);
+//   print(x.contains(100));
+//   print(x);
+  
+//   for(int i in x){
+//     print(i);
+//   }
+  
+//   print(x[x.length-1]);
+  
+//   list of Dynamic
+//   List x=['2',3,4,1,'amira',true,[2,100]];
+// //   print(x);
+// //   genrics
+  
+//   List<int> l=[2,1,4,77];
+//   l=[3,4,5,'omar'];
+  
+//   print(l);
+  //   set
+//   Set<String> x={'apple','banana','kiwi'};
+//    x.add('apple');
+//   print(x.elementAt(0));
+  
+//   x.remove('apple');
+//   print(x.contains('kiwi'));
+
+  //Map
+//   Key:value
+//   Map student1={
+//     'firstName':'ali',
+//     'age':12,
+//     'lastName':'mohamed',
+//     'degree':70
+//   };
+  
+//   student1['age']=13;
+//   student1['hobbies']=['reading','running'];
+// //   print(student1['age']);
+   
+  
+//   student1.remove('degree');
+//   print(student1);
+
+// /nullSaftey
+//   String? name='hossam';
+//   name=null;
+  
+//   print(name??'No Name'); //null-aware
+  
+//   List<int?> l=[2,4,null,8,null];
+  
+  
+//   List<int> newList=l.whereType<int>().toList();
+//   print(newList);
+
+
+
+  
+}
+//entry point
