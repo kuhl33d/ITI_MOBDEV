ac62c530819166e739b3b8bbbd1aa95b00d72672
kuhl33d <khaled.abdelnasser1@msa.edu.eg>
2025-01-30 15:45:11 +0200

    added task 1

--- Day_2/1.dart
+++ Day_2/1.dart
@@ -1,3 +1,27 @@
+import 'dart:io';
+
 void main(){
-  
+  //Create a list of integers: [5, 10, 15, 20, 25].
+  List L = [5,10,15,20,25];
+  print("intial list: ${L}");
+  //Add 30 to the List.
+  L.add(30);
+  print(L);
+  //Remove the first element
+  L.removeAt(0);
+  print(L);
+  //Find all numbers greater than 15.
+  stdout.write("Numbers greater than 15: ");
+  for(int i in L){
+    if(i > 15){
+      stdout.write("${i} ");
+    }
+  }
+  print('');
+  //Multiply all numbers in the list by 2.
+  print("List Before: ${L}");
+  for(int i=0;i<L.length;i++){
+    L[i] *= 2;
+  }
+  print("List After: ${L}");
 }
