7775188683c6cfd74329a6e85c7339ad477c8d92
kuhl33d <khaled.abdelnasser1@msa.edu.eg>
2025-01-31 12:35:44 +0200

    added 10,11

---
+++ Day_2/10.dart
@@ -0,0 +1,13 @@
+//Exercise 10
+
+import 'dart:io';
+
+void main(){
+  //Use a while loop to print all numbers from 10 to 1 in reverse order.
+  int i=10;
+  while(i>=1){
+   stdout.write("${i} ");
+   i--;
+  }
+  print('');
+}
---
+++ Day_2/11.dart
@@ -0,0 +1,13 @@
+//Exercise 11
+
+import 'dart:io';
+
+void main(){
+  //Use a do-while loop to print the numbers from 1 to 5.
+  int i=1;
+  do{
+    stdout.write("${i} ");
+    i++;
+  }while(i<=5);
+  print('');
+}
