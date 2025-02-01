76b07aa6af6d76edef13b8d4ce883b2640479a81
kuhl33d <khaled.abdelnasser1@msa.edu.eg>
2025-02-01 13:55:13 +0200

    added 1 recursive and debug script

--- Day_3/1.dart
+++ Day_3/1.dart
@@ -1,9 +1,19 @@
 //Write a function to check if a given string is a palindrome.
 void main(){
-  List<String> test = ["lol","level","peep","khaled"];
+  List<String> test = ["lol","level","peep","khaled","kick","aabcaa"];
   for(String i in test){
-    print("${i} : ${checkPalindrom(i)}");
+    print("${i} : normal    ${checkPalindrom(i)}");
+    print("${i} : recursive ${recPalin(i,0,i.length-1)}");
   }
+}
+bool recPalin(String S,int i,int j){
+  if(i < S.length/2){
+    if(S[i] != S[j]){
+      return false;
+    }
+    return recPalin(S, i+1, j-1);
+  }
+  return true;
 }
 bool checkPalindrom(String S){
   for(int i=0,j=S.length-1;i<S.length/2;i++,j--){
